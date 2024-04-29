2e3013b3f7a6b4709bc9a2d4c993b63dba925364
idityage <am44910606@gmail.com>
2024-04-29 23:59:43 +0530

    Refactor balance state initialization and API call in Dashboard.jsx

--- paytm/backend/routes/user.js
+++ paytm/backend/routes/user.js
@@ -11,141 +11,141 @@ const userRouter = Router();
 
 // Sign Up Schema
 const signUpSchema = zod.object({
-    username: zod.string().min(3).max(30),
-    password: zod.string().min(6),
-    firstName: zod.string().max(30),
-    lastName: zod.string().max(30),
+	username: zod.string().min(3).max(30),
+	password: zod.string().min(6),
+	firstName: zod.string().max(30),
+	lastName: zod.string().max(30),
 });
 
 // Upadate User Schema
 const updateUserSchema = zod.object({
-    username: zod.string().min(3).max(30),
-    firstName: zod.string().max(30),
-    lastName: zod.string().max(30),
+	username: zod.string().min(3).max(30),
+	firstName: zod.string().max(30),
+	lastName: zod.string().max(30),
 });
 
 // Sign Up Route
 userRouter.post('/signup', async (req, res) => {
-    const { username, password, firstName, lastName } = req.body;
-    const { success } = signUpSchema.safeParse(req.body);
-    if (!success) {
-        return res.status(400).json({ error: "Incorrect Inputs" });
-    }
-    try {
-        const existingUser = await User.findOne({ username })
-        if (existingUser) {
-            return res.status(400).json({ error: "Username already exists" });
-        }
-    } catch (error) {
-        res.status(500).json({ error: "Unable to find user" });
-    }
+	const { username, password, firstName, lastName } = req.body;
+	const { success } = signUpSchema.safeParse(req.body);
+	if (!success) {
+		return res.status(400).json({ error: "Incorrect Inputs" });
+	}
+	try {
+		const existingUser = await User.findOne({ username })
+		if (existingUser) {
+			return res.status(400).json({ error: "Username already exists" });
+		}
+	} catch (error) {
+		res.status(500).json({ error: "Unable to find user" });
+	}
 
-    try {
-        const password_hash = await bcrypt.hash(password, 10);
-        const newUser = await User.create({
-            username,
-            password_hash,
-            firstName,
-            lastName,
-        });
-        const userId = newUser._id;
-        await Account.create({
-            userId,
-            balance: 1 + Math.floor(Math.random() * 10000), // Random balance between 1 and 10000
-        });
-        const token = jwt.sign({
-            userId,
-        }, JWT_SECRET, { expiresIn: '1h' })
+	try {
+		const password_hash = await bcrypt.hash(password, 10);
+		const newUser = await User.create({
+			username,
+			password_hash,
+			firstName,
+			lastName,
+		});
+		const userId = newUser._id;
+		await Account.create({
+			userId,
+			balance: 1 + Math.floor(Math.random() * 10000), // Random balance between 1 and 10000
+		});
+		const token = jwt.sign({
+			userId,
+		}, JWT_SECRET, { expiresIn: '1h' })
 
-        res.status(201).json({
-            token,
-            user: {
-                username: newUser.username,
-                firstName: newUser.firstName,
-                lastName: newUser.lastName,
-            },
-            message: "User created successfully",
-        });
-    } catch (error) {
-        res.status(500).json({ error: "Unable to create user" });
-    }
+		res.status(201).json({
+			token,
+			user: {
+				username: newUser.username,
+				firstName: newUser.firstName,
+				lastName: newUser.lastName,
+			},
+			message: "User created successfully",
+		});
+	} catch (error) {
+		res.status(500).json({ error: "Unable to create user" });
+	}
 })
 
 // Sign In Route
 userRouter.post('/signin', async (req, res) => {
-    try {
-        const { username, password } = req.body;
-        const existingUser = await User.findOne({ username })
-        const isMatch = await bcrypt.compare(password, existingUser.password_hash); // Match --> true, Not Match --> false
-        if (!existingUser || !isMatch) {
-            return res.status(400).json({ error: "Invalid Credentials / Invalid Password" });
-        }
-        const token = jwt.sign({
-            userId: existingUser._id,
-        }, JWT_SECRET, { expiresIn: '1h' })
+	try {
+		const { username, password } = req.body;
+		const existingUser = await User.findOne({ username })
+		const isMatch = await bcrypt.compare(password, existingUser.password_hash); // Match --> true, Not Match --> false
+		if (!existingUser || !isMatch) {
+			return res.status(400).json({ error: "Invalid Credentials / Invalid Password" });
+		}
+		const token = jwt.sign({
+			userId: existingUser._id,
+		}, JWT_SECRET, { expiresIn: '1h' })
 
-        res.status(200).json({
-            token,
-            user: {
-                username: existingUser.username,
-                firstName: existingUser.firstName,
-                lastName: existingUser.lastName,
-            },
-            message: "User signed in successfully",
-        });
-    } catch (error) {
-        res.status(500).json({ error: "Unable to compare password" });
-    }
+		res.status(200).json({
+			token,
+			user: {
+				username: existingUser.username,
+				firstName: existingUser.firstName,
+				lastName: existingUser.lastName,
+			},
+			message: "User signed in successfully",
+		});
+	} catch (error) {
+		res.status(500).json({ error: "Unable to compare password" });
+	}
 })
 
 // Update User Route
 userRouter.put('/update', authMiddleware, async (req, res) => {
-    try {
-        const { username, firstName, lastName } = req.body;
-        const { success } = updateUserSchema.safeParse(req.body);
-        if (!success) {
-            return res.status(400).json({ error: "Incorrect Inputs" });
-        }
-        const user = await User.findOneAndUpdate({ _id: req.userId }, { // req.userId is coming from authMiddleware
-            username,
-            firstName,
-            lastName,
-        }, { new: true });
-        res.status(200).json({
-            user: {
-                username: user.username,
-                firstName: user.firstName,
-                lastName: user.lastName,
-            },
-            message: "User updated successfully",
-        });
-    } catch (error) {
-        res.status(500).json({ error: "Unable to update user" });
-    }
+	try {
+		const { username, firstName, lastName } = req.body;
+		const { success } = updateUserSchema.safeParse(req.body);
+		if (!success) {
+			return res.status(400).json({ error: "Incorrect Inputs" });
+		}
+		const user = await User.findOneAndUpdate({ _id: req.userId }, { // req.userId is coming from authMiddleware
+			username,
+			firstName,
+			lastName,
+		}, { new: true });
+		res.status(200).json({
+			user: {
+				username: user.username,
+				firstName: user.firstName,
+				lastName: user.lastName,
+			},
+			message: "User updated successfully",
+		});
+	} catch (error) {
+		res.status(500).json({ error: "Unable to update user" });
+	}
 })
 
 // Search User Route
 userRouter.get('/bulk', async (req, res) => {
-    try {
-        const filter = req.query.filter || '';
-        const users = await User.find({
-            $or: [ // or --> either of the conditions should be true
-                { username: { $regex: filter, $options: 'i' } }, // i --> case insensitive // regex --> regular expression 
-                { firstName: { $regex: filter, $options: 'i' } },
-                { lastName: { $regex: filter, $options: 'i' } },
-            ]
-        });
-        res.status(200).json({
-            user: users.map(user => ({ // map --> to show all the users which are matching the filter above
-                username: user.username,
-                firstName: user.firstName,
-                lastName: user.lastName,
-                _id: user._id,
-            }))
-        });
-    } catch (error) {
-        res.status(500).json({ error: "Unable to find users" });
-    }
+	try {
+		const filter = req.query.filter || '';
+		const users = await User.find({
+			$or: [ // or --> either of the conditions should be true
+				{ username: { $regex: filter, $options: 'i' } }, // i --> case insensitive // regex --> regular expression 
+				{ firstName: { $regex: filter, $options: 'i' } },
+				{ lastName: { $regex: filter, $options: 'i' } },
+			]
+		});
+		res.status(200).json({
+			user: users.map(user => ({ // map --> to show all the users which are matching the filter above
+				username: user.username,
+				firstName: user.firstName,
+				lastName: user.lastName,
+				_id: user._id,
+			}))
+		});
+	} catch (error) {
+		res.status(500).json({ error: "Unable to find users" });
+	}
 })
 
 
--- paytm/frontend/src/pages/Dashboard.jsx
+++ paytm/frontend/src/pages/Dashboard.jsx
@@ -5,27 +5,27 @@ import { Users } from "../components/Users"
 import axios from "axios";
 
 export const Dashboard = () => {
-    const [balance, setBalance] = useState(0);
-    useEffect(() => {
-        const fetchData = async () => {
-            try {
-                const response = await axios.get("http://localhost:3000/api/v1/account/balance", {
-                    headers: {
-                        "Authorization": "Bearer " + localStorage.getItem("token")
-                    }
-                });
-                setBalance(response.data.balance);
-            } catch (error) {
-                console.error(error);
-            }
-        };
-        fetchData();
-    }, []);
-    return <div>
-        <Appbar />
-        <div className="m-8">
-            <Balance value={balance} />
-            <Users />
-        </div>
-    </div>
+	const [balance, setBalance] = useState(0);
+	useEffect(() => {
+		const fetchData = async () => {
+			try {
+				const response = await axios.get("http://localhost:3000/api/v1/account/balance", {
+					headers: {
+						"Authorization": "Bearer " + localStorage.getItem("token")
+					}
+				});
+				setBalance(response.data.balance);
+			} catch (error) {
+				console.error(error);
+			}
+		};
+		fetchData();
+	}, []);
+	return <div>
+		<Appbar />
+		<div className="m-8">
+			<Balance value={balance} />
+			<Users />
+		</div>
+	</div>
 }
